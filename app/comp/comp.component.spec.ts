3a3c98a7c888942485b3d9153ff555fa135786b8
lavanya57 <45795713+lavanya57@users.noreply.github.com>
2019-04-22 12:11:13 +0530

    today's commit
    up to now creating a contact form and display contact details in dynamic format.
    learnt about Angular4, TypeScript , cards applying validation using bootstrap.

---
+++ app/comp/comp.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CompComponent } from './Comp/CompComponent';
+
+describe('CompComponent', () => {
+  let component: CompComponent;
+  let fixture: ComponentFixture<CompComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CompComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
